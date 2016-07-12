8140afb7954787bcc106d1667e554b7bafdf721b
vdcloud <vivekjdesai@gmail.com>
2016-07-12 16:43:32 +0530

    Initial Commit

---
+++ ContinuousDeploymentDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContinuousDeploymentDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = ConfigurationManager.AppSettings["InstanceName"].ToString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
